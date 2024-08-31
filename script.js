4e7eb3a0e2354673b21a7f323c92fc6f9b520440
andregoobloo <177800443+andregoobloo@users.noreply.github.com>
2024-08-31 13:04:38 -0400

    Rough draft complete, javascript works as intended

--- script.js
+++ script.js
@@ -37,9 +37,15 @@ const displayBook = function (book) {
   bookCard.classList.add("book-card");
 
   // Book title
+  const bookTitleDiv = document.createElement("div");
+  bookTitleDiv.classList.add("book-title-div");
   const bookTitle = document.createElement("h1");
   bookTitle.classList.add("book-title");
   bookTitle.textContent = book.title;
+
+  // Book info div
+  const bookInfoDiv = document.createElement("div");
+  bookInfoDiv.classList.add("book-info-div");
 
   // Book author
   const bookAuthor = document.createElement("h3");
@@ -56,9 +62,9 @@ const displayBook = function (book) {
   bookReleaseYear.classList.add("book-release-year");
   bookReleaseYear.textContent = `Published in ${book.releaseYear}`;
 
-  // Book was read or not
-  const bookHasRead = document.createElement("h3");
-  bookHasRead.classList.add("book-read-status");
+  // Buttons div
+  const bookBtnDiv = document.createElement("div");
+  bookBtnDiv.classList.add("book-btn-div");
 
   // Delete book
   const deleteBookBtn = document.createElement("span");
@@ -69,17 +75,20 @@ const displayBook = function (book) {
   const changeReadStatusBtn = document.createElement("button");
   changeReadStatusBtn.classList.add("change-read-status");
   changeReadStatusBtn.textContent =
-    book.hasRead === true ? "Add to unread" : "Add to read";
+    book.hasRead === true ? "Add to Unread" : "Add to Read";
 
   // Appending
   const appendBookCard = function () {
-    bookCard.append(bookTitle);
-    bookCard.append(bookAuthor);
-    bookCard.append(bookReleaseYear);
-    bookCard.append(bookNumPages);
-    bookCard.append(bookHasRead);
-    bookCard.append(deleteBookBtn);
-    bookHasRead.append(changeReadStatusBtn);
+    bookCard.appendChild(bookTitleDiv);
+    bookCard.append(bookInfoDiv);
+    bookCard.append(bookBtnDiv);
+    bookTitleDiv.append(bookTitle);
+    bookInfoDiv.append(bookAuthor);
+    bookInfoDiv.append(bookReleaseYear);
+    bookInfoDiv.append(bookNumPages);
+    // bookCard.append(bookHasRead);
+    bookBtnDiv.append(changeReadStatusBtn);
+    bookBtnDiv.append(deleteBookBtn);
   };
   if (book.hasRead === true) {
     libraryRead.appendChild(bookCard);
@@ -135,21 +144,21 @@ const deleteBook = function (index) {
   displayTitles();
 };
 
-addBookToLibrary("Kan", "Neena Geena", 323, 1908, false);
-addBookToLibrary("Petper Peep", "Sawyaer Timbo", 500, 1998, true);
-addBookToLibrary("Hunger Games", "Somebody", 350, 2010, true);
-addBookToLibrary("Kan", "Neena Geena", 323, 1908, false);
-addBookToLibrary("Petper Peep", "Sawyaer Timbo", 500, 1998, true);
-addBookToLibrary("Hunger Games", "Somebody", 350, 2010, true);
-addBookToLibrary("Kan", "Neena Geena", 323, 1908, false);
-addBookToLibrary("Petper Peep", "Sawyaer Timbo", 500, 1998, true);
-addBookToLibrary("Hunger Games", "Somebody", 350, 2010, true);
-addBookToLibrary("Kan", "Neena Geena", 323, 1908, false);
-addBookToLibrary("Petper Peep", "Sawyaer Timbo", 500, 1998, true);
-addBookToLibrary("Hunger Games", "Somebody", 350, 2010, true);
-addBookToLibrary("Kan", "Neena Geena", 323, 1908, false);
-addBookToLibrary("Petper Peep", "Sawyaer Timbo", 500, 1998, true);
-addBookToLibrary("Hunger Games", "Somebody", 350, 2010, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 323, 1960, true);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, 1813, false);
+addBookToLibrary("Animal Farm", "George Orwell", 141, 1945, true);
+addBookToLibrary(
+  "The Little Prince",
+  "Antoine de Saint-Exupery",
+  96,
+  1943,
+  false
+);
+addBookToLibrary("1984", "George Orwell", 368, 1949, true);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, 1925, true);
+addBookToLibrary("Lord of the Flies", "William Golding", 182, 1954, false);
+addBookToLibrary("The Kite Runner", "Khaled Hosseini", 371, 2003, true);
+addBookToLibrary("Charlotte's Web", "E.B. White", 184, 1952, false);
 
 displayLibrary();
 
@@ -183,3 +192,43 @@ newBookForm.onsubmit = (e) => {
   newBookDialog.close();
   displayLibrary();
 };
+
+// Sorting books
+const sortBtns = document.querySelector(".sort-btns");
+
+sortBtns.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.textContent === "Pages") {
+    myLibrary.sort((a, b) => a.numPages - b.numPages);
+  }
+  if (e.target.textContent === "Year") {
+    myLibrary.sort((a, b) => a.releaseYear - b.releaseYear);
+  }
+  if (e.target.textContent === "Title") {
+    myLibrary.sort((first, second) => {
+      const firstTitle = first.title.toUpperCase();
+      const secondTitle = second.title.toUpperCase();
+      if (firstTitle < secondTitle) {
+        return -1;
+      } else if (firstTitle > secondTitle) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  if (e.target.textContent === "Author") {
+    myLibrary.sort((first, second) => {
+      const firstAuthor = first.author.toUpperCase();
+      const secondAuthor = second.author.toUpperCase();
+      if (firstAuthor < secondAuthor) {
+        return -1;
+      } else if (firstAuthor > secondAuthor) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  displayLibrary();
+});
